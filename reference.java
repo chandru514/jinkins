5dc903b893314cc6857649a9f9348bcbe10689d6
Chandru514 <chandru.m2024@vitstudent.ac.in>
2026-09-04 22:44:45 +0530

    first commit

---
+++ reference.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+public class reference {
+   public static void main(String []args){
+    byte a=10;
+    Date now=new Date();
+    System.out.println("Byte: " + a);
+    System.out.println("Date: " + now);
+    
+
+    
+   }
+     
+}
--- variable.java
+++ variable.java
@@ -9,6 +9,5 @@ public class variable {
             System.out.println("String: " + b);
         }
     }
-
-    
+     
 }

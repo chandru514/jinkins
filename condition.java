1e690ef69097501a22258e5eb4c6c1c005d453b7
Chandru514 <chandru.m2024@vitstudent.ac.in>
2026-09-05 22:07:03 +0530

    Updated code

---
+++ condition.java
@@ -0,0 +1,47 @@
+public class condition {
+    public static void main(String[]args){
+        int a=10;
+        int b=20;
+        if(a>b){
+            System.out.println("a is greater than b");
+        }
+        else if(a<b){
+            System.out.println("a is less than b");
+        }
+        else if(a==b){
+            System.out.println("a is equal to b");
+        }
+        else{
+            System.out.println("Invalid");
+        }
+
+        int day =1;
+        switch(day){
+            case 1:
+                System.out.println("sunday");
+                break;
+            case 2:
+                System.out.println("monday");
+                break;
+            case 3:
+                System.out.println("tuesday");
+                break;  
+            case 4:
+                System.out.println("wednesday");
+                break;  
+            case 5:
+                System.out.println("thursday");     
+                break;
+            case 6:
+                System.out.println("friday");
+                break;
+            case 7:
+                System.out.println("saturday");
+                    break;
+                    default:
+                        System.out.println("invalid");
+        }
+    }
+}
+
+        

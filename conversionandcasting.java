d23052ab0dfcc78831b6eb157bc803dccbc963e0
Chandru514 <chandru.m2024@vitstudent.ac.in>
2026-09-05 14:01:15 +0530

    Added Java code

---
+++ conversionandcasting.java
@@ -0,0 +1,15 @@
+public class conversionandcasting {
+    public static void main(String[]args){
+        String person1 ="vit";
+        String person2="srm";
+        String person3=person1;
+        System.out.println("Person 1: " + person1);
+        System.out.println("Person 2: " + person2);
+        System.out.println("Person 3: " + person3);
+        double num1= 10.5;
+        int num2 =(int)num1;
+        System.out.println("Num 1:" + num2);
+
+    }
+    
+}

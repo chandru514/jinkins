4d85e270377ce40ae13d3c2430c78eb4add9f9a2
Chandru514 <chandru.m2024@vitstudent.ac.in>
2026-09-05 13:38:44 +0530

    Added Java code

---
+++ literals.java
@@ -0,0 +1,9 @@
+public class literals {
+    public static void main(String[] args) {
+       char a = 'A';
+         System.out.println("Char a: " + a);
+         a++;
+         System.out.println("Char b: " + a);
+    
+}
+}
---
+++ primitiveorreference.java
@@ -0,0 +1,11 @@
+public class primitiveorreference {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = a;
+        a=20;
+        System.out.println("Byte a: " + a);
+        System.out.println("Byte b: " + b);
+
+    
+}
+}

f32a9ea5fb09bfcd69b2544175c27d30ce0bce23
Chandru514 <chandru.m2024@vitstudent.ac.in>
2026-09-06 13:04:32 +0530

    added code

---
+++ function.java
@@ -0,0 +1,12 @@
+public class function {
+    static void hello(){
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+         for (int i=0;i<5;i++){
+            System.out.println("hello");
+    }
+    
+}
+}
---
+++ inputnoreturn.java
@@ -0,0 +1,12 @@
+public class inputnoreturn {
+    static void print(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5,6,7,8};
+        print(arr);
+    }
+    
+}

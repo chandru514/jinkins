e8d7408581d2f9c10e19c0af88c4e34757fbccd5
Chandru514 <chandru.m2024@vitstudent.ac.in>
2026-09-09 11:58:40 +0530

    updated code

---
+++ forloop1.java
@@ -0,0 +1,13 @@
+public class forloop1 {
+    public static void main(String []a){
+        for(int i=1;i<=7;i++){
+            System.out.println("Day"+i);
+                for(int j=1;j<=9;j++)
+                    {
+                    System.out.println(" "+(j+8)+"-"+(j+9));
+                }
+            }
+        }
+    }
+    
+
---
+++ whileloop.java
@@ -0,0 +1,12 @@
+public class whileloop {
+    public static void main(String[]args){
+        int i=10;
+        while(i%2==0){
+            System.out.println("Even"+i);
+            i++;
+        }
+        System.out.println("Bye"+i);
+    }
+
+    
+}
